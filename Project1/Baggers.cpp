99340fb7fe13dabc4b1eab0073969be323d2472d
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-02-27 10:08:27 -0800

    project 1

---
+++ Project1/Baggers.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+Baggers::Baggers()
+{
+  _name = "Null";
+}
+
+Baggers::Baggers(string _name, string _number) : _name(name) , _number(number)
+{
+
+}
+
+Baggers::~Baggers()
+{
+
+}
---
+++ Project1/Baggers.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+class Baggers
+{
+private:
+  std:: _name;
+  std:: _number;
+  
+public:
+  Baggers();
+  Baggers(std::string _name);
+  
+  ~Baggers();
+  
+  std::GetName();
+  std::GetNumber();
+};
