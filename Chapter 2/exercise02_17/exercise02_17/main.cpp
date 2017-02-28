a155e090e6d61ef82a41434ce889331610e17907
Trevis Jontaé Fields <tfields3@student.rcc.edu>
2017-02-28 04:38:15 -0800

    exercise 2.17(printing)
    Write a program that prints the numbers 1 to 4 on the same line with
    each pair of adjacent numbers separated by one space. do this several
    ways:
    a) Using one statement with one stream insertion operator
    b) Using one statement with four stream insertion operators
    c) Using four statements

---
+++ Chapter 2/exercise02_17/exercise02_17/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //using one stream insertion operator
+  cout << "1 2 3 4\n";
+  //using for stream insertion operators
+  cout << "1" <<" 2" << " 3" << " 4" << endl;
+  //using four statements
+  cout << "1 ";
+  cout << "2 ";
+  cout << "3 ";
+  cout << "4\n";
+}
